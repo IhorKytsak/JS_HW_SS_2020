28f10b7e84dee6765b2498623a2604ee9c4944d5
Ihor Kytsak <ihor.kytsak@ukr.net>
2020-01-14 19:19:29 +0200

    homework1

---
+++ H1/t5.js
@@ -0,0 +1,18 @@
+let name;
+let lastName;
+let trainingGroup;
+let birthday;
+name = 'Ihor';                                // 'string'
+lastName = 'Kytsak';                          // 'string'
+trainingGroup = 'Lv-476.JS Core';             // 'string'
+birthday = 1996;                              // 'number'
+let marriage = confirm('Are you married?');   // 'boolean'
+console.log(typeof birthday);
+console.log(typeof marriage);
+console.log(typeof name);
+console.log(typeof lastName);
+console.log(typeof trainingGroup);
+let data = null;
+let data2 ;
+console.log(typeof data);
+console.log(typeof data2);
